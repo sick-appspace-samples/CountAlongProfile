eb4bbe8e6c429cce222eaeacb31f7d97b729668f
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 13:03:05 +0200

    'Update'

--- CountAlongProfile/scripts/countAlongProfile.lua
+++ CountAlongProfile/scripts/countAlongProfile.lua
@@ -1,27 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  CountAlongProfile
-
-  Summary:
-  Counting number of tea bags in a box
-
-  Description:
-  This sample demonstrates how to use the Profile API to count and verify the
-  number of tea bags in a box.
-
-  How to run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the image viewer on the DevicePage.
-  Restarting the Sample may be necessary to show the images and profiles after loading
-  the webpage.
-  To run this Sample a device with SICK Algorithm API and AppEngine >= V2.11 is
-  required. For example SIM4000 with latest firmware. Alternatively the Emulator
-  on AppStudio 3.1.1 or higher can be used.
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 -- Create and configure decorators
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 ## CountAlongProfile
-Extracting a profile from an image to be able to count and verify the correct number of items (tea bags).
+Extracting a profile from an image to be able to count and verify the correct number of items (tea bags.
 ### Description
-This Sample shows how to use the algorithms of the Profile API to count and verify the number of tea bags in a box. 
+This Sample shows how to use the algorithms of the Profile API to count and verify the number of tea bags in a box.
+
 ### How to Run
 Starting this sample is possible either by running the App (F5) or debugging (F7+F10). Setting breakpoint on the first row inside the 'main' function allows debugging step-by-step after 'Engine.OnStarted' event. Results can be seen in the image viewer on the DevicePage.
 Restarting the Sample may be necessary to show the profiles after loading the web-page.
